7d0516da8c9b5609a80bc4542f6a4b8e54b243a2
deamsf <frankdamen@gmail.com>
2025-03-23 14:40:03 +0100

    to convert

---
+++ src/constants/navigation.ts
@@ -0,0 +1,8 @@
+export const navigation = {
+  links: [
+    { label: 'Wat ik doe', href: '#services' },
+    { label: 'Over mij', href: '#about' },
+    { label: 'Klanten', href: '#clients' },
+    { label: 'Contact', href: '#contact' }
+  ]
+} as const;
